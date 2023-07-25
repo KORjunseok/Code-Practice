26565f7e289874952f7579b723984243ce8bc566
Oh Junseok <y22zzp@naver.com>
2023-07-25 23:49:14 +0900

    혼공자 4챕터 시작

---
+++ Hon-gong-JS/04.chapter/04-01.js
@@ -0,0 +1,14 @@
+// 1번
+const fruits = ["배", "사과", "키위", "바나나"];
+console.log(fruits[fruits.length - 1]);
+
+// 2번
+const todos = ["우유구매", "업무 메일 확인하기", "필라테스 수업"];
+todos.push("저녁 식사 준비하기");
+todos.push("피아노 연습하기");
+
+// 3번
+const fruitsB = ["사과", "배", "바나나"];
+fruitsB[fruitsB.length] = "귤";
+
+console.log(fruitsB);
