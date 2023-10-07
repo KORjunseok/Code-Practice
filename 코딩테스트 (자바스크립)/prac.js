30cf73a7894c32bd8a211c92f627835bcdfe5c46
Oh Junseok <y22zzp@naver.com>
2023-10-08 02:41:24 +0900

    코딩테스트 01일차

---
+++ 코딩테스트 (자바스크립)/prac.js
@@ -0,0 +1,7 @@
+a = 7;
+b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b)); // 몫만 남기기
+console.log(a % b);
---
+++ 코딩테스트 (자바스크립)/코딩 테스트용 JavaScript 빠른 출력.js
@@ -0,0 +1,6 @@
+let answer = '';
+
+for (let i = 1; i <= 100; i++) {
+answer += i + '\n'; // 문자열로 변환하여 기록
+}
+console.log(answer);
